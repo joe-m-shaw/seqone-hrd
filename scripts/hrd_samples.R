1ff454615909e4832eb0279db154c2c7b170668c
Shaw Joseph (R0A) MFT <Joseph.Shaw2@mft.nhs.uk>
2023-09-13 15:13:19 +0100

    Extract values from SeqOne PDFs

--- scripts/hrd_samples.R
+++ scripts/hrd_samples.R
@@ -3,8 +3,6 @@
 # joseph.shaw3@nhs.net
 ################################################################################
 
-rm(list=ls())
-
 ##################################################
 # Packages
 ##################################################
@@ -25,10 +23,16 @@ sample_volumes <- read_csv(paste0(homepath, "data/hrd_sample_volumes.csv")) %>%
 seqone_run1 <- read_csv(paste0(homepath, "data/seqone_run1.csv")) %>%
   filter(!base::duplicated(specimen_number))
 
+# Sample spreadsheet sent by Eleanor Baker
 sample_info <- read_excel(paste0(homepath, "data/HRD Trial samples - QCs added(AutoRecovered).xlsx"),
                           sheet = "Sheet1") %>%
   janitor::clean_names() %>%
   mutate(specimen_number = as.numeric(lab_no))
+
+# Sample information exported from DLMS with disease code 204
+dlms_info <- read_csv(paste0(homepath, "data/DDBK_Samples_204.csv")) %>%
+  janitor::clean_names() %>%
+  dplyr::rename(specimen_number = labno)
 
 ##################################################
 # Join Sample Information
@@ -38,12 +42,33 @@ collated_sample_info <- sample_volumes %>%
   dplyr::rename(volume_ul_12_09_2023 = volume_ul) %>%
   mutate(seqone_run1 = ifelse(specimen_number %in% seqone_run1$specimen_number, 
          "Yes", "No")) %>%
-  left_join(sample_info, by = "specimen_number") %>%
-  select(-c(lab_no, sample_stratergy, date_reported, x12, x14, volume,
-            conc, dqn))
+  left_join(dlms_info, by = "specimen_number")
 
-write.csv(collated_sample_info, 
-          file = paste0(homepath, "outputs/collated_hrd_information.csv"),
-          row.names = FALSE)
+##################################################
+# Extract neoplastic cell content using regex
+##################################################
+
+# Can be NCC or TCC
+comment_regex_single <- ".+(\\W{1}\\d{2}%).+"
+
+comment_regex_range <- ".+(\\d{2}\\W{1}\\d{2}%).+"
+
+check <- collated_sample_info %>%
+  mutate(ncc_single = sub(x = comments,
+                   pattern = comment_regex_single,
+                   replacement = "\\1"),
+         ncc_range = sub(x = comments,
+                          pattern = comment_regex_range,
+                          replacement = "\\1"),
+         ncc_final = ifelse(length(ncc_range) == 6, ncc_range, ncc_single)) %>%
+  select(specimen_number, comments, ncc_single, ncc_range, ncc_final)
+
+##################################################
+# Export
+##################################################
+
+#write.csv(collated_sample_info, 
+          #file = paste0(homepath, "outputs/collated_hrd_information.csv"),
+          #row.names = FALSE)
 
 ##################################################
--- scripts/seq_one_analysis.R
+++ scripts/seq_one_analysis.R
@@ -16,11 +16,97 @@ library(tidyverse)
 # Read PDFs
 ##################################################
 
-location <- "~/homologous_recombination_deficiency/inputs/"
+location <- "~/homologous_recombination_deficiency/data/seqone_reports/"
 
-report <- pdftools::pdf_text(pdf = paste0(location, "seqone-hrd-example-report-positive-brca", ".pdf")) %>% 
-  strsplit(split = "\n")
+seqone_reports <- list.files(location)
 
-pdftools::pdf_data(pdf = paste0(location, "seqone-hrd-example-report-positive-brca", ".pdf"))
+collated_info <- data.frame()
+
+for (i in seqone_reports) {
+  
+  report_data <- pdftools::pdf_data(pdf = paste0(location, i))
+  
+  # Page 1, list 6 for text
+  page1_text <- report_data[[1]][[6]]
+  
+  # Each value has its own coordinate within the PDF
+  hrd_score <- as.numeric(page1_text[[197]])
+  
+  status <- page1_text[[198]]
+  
+  lga <- page1_text[[160]]
+  
+  lpc <- page1_text[[163]]
+  
+  ccne1 <- page1_text[[166]]
+  
+  rad51b <- page1_text[[169]]
+  
+  ncc <- page1_text[[108]]
+  
+  coverage <- page1_text[[123]]
+  
+  percent_mapping <- page1_text[[135]]
+  
+  sample <- page1_text[[24]]
+  
+  tmp_output <- data.frame("sample" = sample,
+                           "hrd_score" = hrd_score,
+                           "status" = status,
+                           "lga" = lga,
+                           "lpc" = lpc,
+                           "ccne1" = ccne1,
+                           "rad51b" = rad51b,
+                           "ncc" = ncc,
+                           "coverage" = coverage,
+                           "percent_mapping" = percent_mapping)
+  
+  collated_info <- rbind(collated_info, tmp_output)
+  
+  rm(tmp_output)
+  rm(report_data)
+}
+
+##################################################
+# Modify collated information
+##################################################
+
+collated_edit <- collated_info %>%
+  filter(sample != "performed") %>%
+  mutate(specimen_number = as.numeric(sub(x = sample,
+                               pattern = "WS\\d{6}_(\\d{8})\\D{0,1}",
+                               replacement = "\\1"))) %>%
+  filter(specimen_number != 21012359)
+
+##################################################
+# Compare to Myriad results
+##################################################
+
+source("scripts/hrd_samples.R")
+
+seqone_vs_myriad <- collated_edit %>%
+  inner_join(sample_info, by = "specimen_number")
+
+comparison_plot <- ggplot(seqone_vs_myriad, aes(x = gis_score, y = hrd_score)) +
+  geom_point(size = 2, alpha = 0.5) +
+  theme_bw() +
+  scale_x_continuous(limits = c(0,100),
+                     breaks = c(0, 25, 42, 50, 75, 100)) +
+  ylim(0, 1) +
+  labs(x = "Myriad Genome Instability Score",
+       y = "SeqOne HRD Score",
+       title = "Comparison of Myriad vs SeqOne HRD Testing") +
+  geom_vline(xintercept = 42, linetype = "dashed") +
+  geom_hline(yintercept = 0.5, linetype = "dashed")
+
+ggsave(plot = comparison_plot, 
+       filename = paste0("comparison_plot_",
+                         format(Sys.time(), "%Y%m%d_%H%M%S"),
+                         ".tiff"),
+       path = "~/homologous_recombination_deficiency/plots/", 
+       device='tiff',
+       units = "cm",
+       width = 15,
+       height = 15)
 
 ##################################################
