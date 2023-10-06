e282eaf42db981f13e269d3ba77ac19846c1685f
Shaw Joseph (R0A) MFT <Joseph.Shaw2@mft.nhs.uk>
2023-10-06 16:15:52 +0100

    Add code stub for Chris Kershaw cases

---
+++ scripts/chris_cases.R
@@ -0,0 +1,23 @@
+################################################################################
+# Finding GEL Samples for Chris
+################################################################################
+
+rm(list=ls())
+
+source("scripts/dlms_connection.R")
+
+sample_columns <- sqlColumns(channel = moldb_connection,
+           catalog = "MolecularDB",
+           schema = "dbo",
+           sqtable = "Samples")
+
+# Add sample IDs as an input csv to avoid committing identifiers.
+
+sample_query <- paste0("SELECT * FROM MolecularDB.dbo.Samples WHERE NGIS IN (",
+                       paste(chris_cases, collapse = ", "),
+                       ")")
+
+sqlQuery(channel = moldb_connection,
+                        query = sample_query)
+
+################################################################################
