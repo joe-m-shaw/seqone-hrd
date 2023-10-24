f533cf84817e5e2542cda670b95af2aab366de02
Shaw Joseph (R0A) MFT <Joseph.Shaw2@mft.nhs.uk>
2023-10-24 13:30:10 +0100

    Add report location paths

--- scripts/hrd_filepaths.R
+++ scripts/hrd_filepaths.R
@@ -10,4 +10,8 @@ hrd_output_path <- paste0(hrd_project_path, "outputs/")
 
 hrd_plot_path <- paste0(hrd_project_path, "plots/")
 
+myriad_reports_location <- paste0(hrd_data_path, "myriad_reports/")
+
+seqone_report_location <- paste0(hrd_data_path, "seqone_reports/")
+
 ##################################################
