74c176ab6a76f895f43a389b2df4ebad9d4c0b9f
Shaw Joseph (R0A) MFT <Joseph.Shaw2@mft.nhs.uk>
2023-10-25 09:44:51 +0100

    Update project filepaths

--- scripts/hrd_filepaths.R
+++ scripts/hrd_filepaths.R
@@ -1,8 +1,11 @@
-################################################################################
 # HRD project filepaths
-################################################################################
 
-hrd_project_path <- "S:/central shared/Genetics/Mol_Shared/Development.Team/SeqOne Homologous Recombination Deficiency Validation/HRD R script files/"
+dev_team_path <- "S:/central shared/Genetics/Mol_Shared/Development.Team/"
+
+seqone_folder <- paste0(dev_team_path, 
+                        "SeqOne Homologous Recombination Deficiency Validation/")
+
+hrd_project_path <- paste0(seqone_folder, "HRD R script files/")
 
 hrd_data_path <- paste0(hrd_project_path, "data/")
 
@@ -13,5 +16,3 @@ hrd_plot_path <- paste0(hrd_project_path, "plots/")
 myriad_reports_location <- paste0(hrd_data_path, "myriad_reports/")
 
 seqone_report_location <- paste0(hrd_data_path, "seqone_reports/")
-
-##################################################
